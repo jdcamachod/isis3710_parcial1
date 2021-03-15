5e2472630cea65f75a062f28f70c872171152007
Juan Diego Camacho Daza <diego@Juans-MacBook-Pro.local>
2021-03-14 23:54:31 -0500

    entrega final

--- main.js
+++ main.js
@@ -15,8 +15,17 @@ const loadByCategory = (button, card, prodName, products, shoppingCar, car, card
             auxCard.children[0].children[1].children[1].innerHTML = element.description;
             auxCard.children[0].children[1].children[2].innerHTML = `$${element.price}`;
             auxCard.children[0].children[1].children[3].addEventListener("click", () => {
-                shoppingCar.push(element);
-                car.innerHTML = `${shoppingCar.length} items`;
+                let esta = false;
+                shoppingCar.forEach((aux) => {
+                    if (aux.description === element.name) {
+                        aux.quantity++;
+                        esta = true;
+                    }
+                });
+                if (!esta) {
+                    shoppingCar.push({ item: shoppingCar.length+1, quantity: 1, description: element.name, unitPrice: element.price });
+                    car.innerHTML = `${shoppingCar.length} items`;
+                }
             });
             cardList.appendChild(auxCard);
         });
@@ -32,11 +41,10 @@ fetch(url).then(res => res.json()).then(res => {
     let salads;
     let desserts;
     let drinkSides;
-    const shoppingCar = new Array();
+    let shoppingCar = new Array();
     const car = document.getElementById("items");
-    const exRow = document.querySelector("#compra");
+    const exRow = document.querySelectorAll("#compra");
     products.forEach(element => {
-        console.log(element);
         if (element.name === "Burguers") {
             burguers = [...element.products];
         }
@@ -63,8 +71,9 @@ fetch(url).then(res => res.json()).then(res => {
     const tableStriped = document.querySelector(".table-striped");
     const tabBody = document.querySelector("#tab-body");
     const section = document.querySelector("#Burgers");
+    const confButtons = document.querySelector("#buttons");
+    confButtons.remove();
     tabla.remove();
-    console.log(tabla);
     document.querySelectorAll(".carta").forEach((element) => {
         element.remove();
     });
@@ -76,8 +85,17 @@ fetch(url).then(res => res.json()).then(res => {
         auxCard.children[0].children[1].children[1].innerHTML = element.description;
         auxCard.children[0].children[1].children[2].innerHTML = `$${element.price}`;
         auxCard.children[0].children[1].children[3].addEventListener("click", () => {
-            shoppingCar.push(element);
-            car.innerHTML = `${shoppingCar.length} items`;
+            let esta = false;
+            shoppingCar.forEach((aux) => {
+                if (aux.description === element.name) {
+                    aux.quantity++;
+                    esta = true;
+                }
+            });
+            if (!esta) {
+                shoppingCar.push({ item: shoppingCar.length, quantity: 1, description: element.name, unitPrice: element.price });
+                car.innerHTML=`${shoppingCar.length} items`;
+            }
         });
         cardList.appendChild(auxCard);
     });
@@ -110,25 +128,98 @@ fetch(url).then(res => res.json()).then(res => {
             document.querySelectorAll(".carta").forEach((element) => {
                 element.remove();
             });
-            exRow.remove();
+            const exRow0 = exRow[0];
             
-            let numItem = 1;
+            exRow.forEach((element) => { element.remove(); });
+            let total = 0;
             shoppingCar.forEach((element) => {
-                console.log(element);
-                const newRow = exRow.cloneNode(true);
-                newRow.children[0].innerHTML = numItem;
-                numItem++;
-                newRow.children[1].innerHTML = 1;
-                newRow.children[2].innerHTML = element.name;
-                newRow.children[3].innerHTML = element.price;
-                newRow.children[4].innerHTML = element.price * 1;
-                newRow.children[5].innerHTML = "pendiente";
+                const newRow = exRow0.cloneNode(true);
+                newRow.children[0].innerHTML = element.item;
+                newRow.children[1].innerHTML = element.quantity;
+                newRow.children[2].innerHTML = element.description;
+                newRow.children[3].innerHTML = element.unitPrice;
+                newRow.children[4].innerHTML = element.unitPrice * element.quantity;
+                newRow.children[5].children[0].addEventListener("click", () => {
+                    if (element.quantity > 1) {
+                        element.quantity--;
+                        newRow.children[1].innerHTML = element.quantity;
+                        newRow.children[4].innerHTML = element.quantity * element.unitPrice;
+                    }
+                });
+                newRow.children[5].children[1].addEventListener("click", () => {
+                    
+                    element.quantity++;
+                    newRow.children[1].innerHTML = element.quantity;
+                    newRow.children[4].innerHTML = element.quantity * element.unitPrice;
+                    
+                });
+
+                total += element.unitPrice * element.quantity;
                 tabBody.appendChild(newRow);
                 tableStriped.appendChild(tabBody);
                 tabla.append(tableStriped);
-                console.log(newRow.children);
             });
-            section.append(tabla);
+            section.appendChild(tabla);
+            confButtons.children[0].children[0].innerHTML = `Total $${total}`;
+            confButtons.children[0].children[1].children[0].addEventListener("click", () => {
+                console.log(shoppingCar);
+                shoppingCar = new Array();
+                car.innerHTML = `${shoppingCar.length} items`;
+                tabla.remove();
+                confButtons.remove();
+                burguers.forEach((element) => {
+                    prodName.innerHTML = "Burguers";
+                    const auxCard = card.cloneNode(true);
+                    auxCard.children[0].children[0].src = element.image;
+                    auxCard.children[0].children[1].children[0].innerHTML = element.name;
+                    auxCard.children[0].children[1].children[1].innerHTML = element.description;
+                    auxCard.children[0].children[1].children[2].innerHTML = `$${element.price}`;
+                    auxCard.children[0].children[1].children[3].addEventListener("click", () => {
+                        let esta = false;
+                        shoppingCar.forEach((aux) => {
+                            if (aux.description === element.name) {
+                                aux.quantity++;
+                                esta = true;
+                            }
+                        });
+                        if (!esta) {
+                            shoppingCar.push({ item: shoppingCar.length, quantity: 1, description: element.name, unitPrice: element.price });
+                            car.innerHTML = `${shoppingCar.length} items`;
+                        }
+                    });
+                    cardList.appendChild(auxCard);
+                });
+                
+            });
+            confButtons.children[0].children[1].children[1].addEventListener("click", () => {
+                tabla.remove();
+                tabBody.remove();
+                tableStriped.remove();
+                confButtons.remove();
+                burguers.forEach((element) => {
+                    prodName.innerHTML = "Burguers";
+                    const auxCard = card.cloneNode(true);
+                    auxCard.children[0].children[0].src = element.image;
+                    auxCard.children[0].children[1].children[0].innerHTML = element.name;
+                    auxCard.children[0].children[1].children[1].innerHTML = element.description;
+                    auxCard.children[0].children[1].children[2].innerHTML = `$${element.price}`;
+                    auxCard.children[0].children[1].children[3].addEventListener("click", () => {
+                        let esta = false;
+                        shoppingCar.forEach((aux) => {
+                            if (aux.description === element.name) {
+                                aux.quantity++;
+                                esta = true;
+                            }
+                        });
+                        if (!esta) {
+                            shoppingCar.push({ item: shoppingCar.length, quantity: 1, description: element.name, unitPrice: element.price });
+                            car.innerHTML = `${shoppingCar.length} items`;
+                        }
+                    });
+                    cardList.appendChild(auxCard);
+                }); 
+            });
+            section.appendChild(confButtons);
             
         }
         
